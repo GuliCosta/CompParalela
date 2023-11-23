3ebb5bc6bf5af40a6940a2c388cea62cfe95a62c
GuliCosta <125361336+GuliCosta@users.noreply.github.com>
2023-11-23 17:07:33 -0300

    Update ProJF.c

--- ProjF/ProJF.c
+++ ProjF/ProJF.c
@@ -4,70 +4,70 @@
 #include <omp.h>
 #include <time.h>
 
-void factorial_list(mpf_t *list, int n);
+void calcular_fatorial(mpf_t *lista, int n);
 
 int main()
 {
-    int n = 10000;
-    mpf_t euler, fat, temp;
-    mpf_t *listfat = malloc((n + 1) * sizeof(mpf_t));
+    int limite = 10000;
+    mpf_t euler, fatorial_atual, temp;
+    mpf_t *lista_fatoriais = malloc((limite + 1) * sizeof(mpf_t));
     mpf_set_default_prec(1000000);
 
     mpf_init_set_ui(euler, 1);
-    mpf_init_set_ui(fat, 1);
-    mpf_init(temp); // Move a inicialização de temp para fora do loop
+    mpf_init_set_ui(fatorial_atual, 1);
+    mpf_init(temp); // Movendo a inicialização de temp para fora do loop
 
-    factorial_list(listfat, n);
+    calcular_fatorial(lista_fatoriais, limite);
     gmp_printf("Lista Pronta\n");
 
-    clock_t start_time = clock();
+    clock_t tempo_inicio = clock();
 
     #pragma omp parallel for reduction(+:euler) private(temp)
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= limite; i++)
     {
         mpf_init_set_ui(temp, 1); // Inicializa temp dentro do loop
 
-        mpf_div(temp, temp, listfat[i]);
+        mpf_div(temp, temp, lista_fatoriais[i]);
         mpf_add(euler, euler, temp);
     }
 
-    clock_t end_time = clock();
+    clock_t tempo_fim = clock();
 
     gmp_printf("Euler = %.35655Ff\n", euler);
 
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // Corrige o cálculo do tempo
+    double tempo_decorrido = (double)(tempo_fim - tempo_inicio) / CLOCKS_PER_SEC; // Corrige o cálculo do tempo
 
-    printf("Elapsed time: %.1f seconds\n", elapsed_time);
+    printf("Tempo decorrido: %.1f segundos\n", tempo_decorrido);
 
     mpf_clear(euler);
-    mpf_clear(fat);
+    mpf_clear(fatorial_atual);
     mpf_clear(temp);
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= limite; i++)
     {
-        mpf_clear(listfat[i]);
+        mpf_clear(lista_fatoriais[i]);
     }
-    free(listfat);
+    free(lista_fatoriais);
 
     return 0;
 }
 
-void factorial_list(mpf_t *list, int n)
+void calcular_fatorial(mpf_t *lista, int n)
 {
     mpf_t temp;
     mpf_init(temp);
 
     for (int i = 0; i <= n; i++)
     {
-        mpf_init(list[i]);
+        mpf_init(lista[i]);
         mpf_set_ui(temp, i);
         if (i > 0)
         {
-            mpf_mul(list[i], list[i - 1], temp);
+            mpf_mul(lista[i], lista[i - 1], temp);
         }
         else
         {
-            mpf_set_ui(list[i], 1);
+            mpf_set_ui(lista[i], 1);
         }
     }
 
